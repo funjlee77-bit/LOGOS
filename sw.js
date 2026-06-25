e3ad47a5badff50049498ea4a09075c7d85a7b94
funjlee77-bit <funjlee77@gmail.com>
2026-06-25 14:59:55 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,11 @@
-const CACHE_NAME = 'bible-memorize-v3';
-const urlsToCache = ['./index.html', './manifest.json'];
+const CACHE_NAME = 'bible-memorize-v4';
+const urlsToCache = [
+  './index.html',
+  './manifest.json',
+  'https://unpkg.com/react@18/umd/react.production.min.js',
+  'https://unpkg.com/react-dom@18/umd/react-dom.production.min.js',
+  'https://cdn.tailwindcss.com'
+];
 
 self.addEventListener('install', event => {
   event.waitUntil(
@@ -20,6 +26,17 @@ self.addEventListener('activate', event => {
 self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request)
-      .then(response => response || fetch(event.request))
+      .then(response => {
+        if (response) return response;
+        return fetch(event.request).then(fetchResponse => {
+          if (fetchResponse && fetchResponse.status === 200) {
+            const responseClone = fetchResponse.clone();
+            caches.open(CACHE_NAME).then(cache => {
+              cache.put(event.request, responseClone);
+            });
+          }
+          return fetchResponse;
+        });
+      })
   );
 });
